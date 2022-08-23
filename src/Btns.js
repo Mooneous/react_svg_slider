418766590684ce497b5503b66a2549ac186c65f2
Mooneous <4087022@naver.com>
2022-08-23 18:22:07 +0900

    모션중 재이벤트 방지(광클릭방지)

--- src/Btns.js
+++ src/Btns.js
@@ -1,5 +1,9 @@
+import { useRef } from 'react';
+
 function Btns(props) {
 	const delay = 800;
+	const enableClick = useRef(true);
+
 	const getItems = (item) => {
 		const panel = item.current;
 		const panel_items = panel.children;
@@ -10,18 +14,14 @@ function Btns(props) {
 		return [panel, panel_items, len, current_item, current_index];
 	};
 
+	//현재enableClick이true가아니면패널이모션중이니까여기서끝~true면 enableClick을false상태로변경하고아래코드실행~~
 	const moveUp = () => {
+		if (!enableClick.current) return;
+		enableClick.current = false;
 		const [panel, panel_items, len, current_item, current_index] = getItems(props.panel);
-		//console.log('panel', panel);
-		//console.log('panel_items', panel_items);
-		//console.log('len', len);
-		//console.log('current_item', current_item);
-		//console.log('current_index', current_index);
-		//len값(li의개수4)-1은 3, 3이 현재인덱스값과 같지않으면 인덱스값+1하고, 같으면 마지막패널이니까 0으로 만들어줘~
 		let next_index = null;
 		current_index !== len - 1 ? (next_index = current_index + 1) : (next_index = 0);
 
-		//기존 활성화 패널을 위로 올려서 숨기고 앞으로 활성화될 패널을 아래쪽에 배치해서 초기화
 		current_item.classList.remove('on');
 		current_item.classList.add('up');
 		panel_items[next_index].classList.add('down');
@@ -30,16 +30,20 @@ function Btns(props) {
 			panel_items[next_index].classList.remove('down');
 			panel_items[next_index].classList.add('on');
 			panel.querySelector('.up').classList.remove('up');
+			//delay시간뒤에 클릭할수있어?있어!로 상태변경
+			setTimeout(() => {
+				enableClick.current = true;
+			}, delay);
 		}, delay);
 	};
 
 	const moveDown = () => {
+		if (!enableClick.current) return;
+		enableClick.current = false;
 		const [panel, panel_items, len, current_item, current_index] = getItems(props.panel);
-		//현재 인덱스값이 0이 아니면 첫패널 아니므로 -1해주고, 0이면 첫패널인거고 len-1=3으로 그니까 마지막패널로 가즈아~
 		let prev_index = null;
 		current_index !== 0 ? (prev_index = current_index - 1) : (prev_index = len - 1);
 
-		//기존 활성화 패널을 아래로 내려서 숨기고 앞으로 활성화될 패널을 위쪽에 배치해서 초기화
 		current_item.classList.remove('on');
 		current_item.classList.add('down');
 		panel_items[prev_index].classList.add('up');
@@ -48,6 +52,10 @@ function Btns(props) {
 			panel_items[prev_index].classList.remove('up');
 			panel_items[prev_index].classList.add('on');
 			panel.querySelector('.down').classList.remove('down');
+
+			setTimeout(() => {
+				enableClick.current = true;
+			}, delay);
 		}, delay);
 	};
 
