3e77c1a86a0408183048222550c1276eeec8963d
Mooneous <4087022@naver.com>
2022-08-23 16:24:43 +0900

    btnUp클릭시 props.panel로 부터 활성화 패널 정보 가져오기

--- src/Btns.js
+++ src/Btns.js
@@ -1,7 +1,25 @@
-function Btns() {
+function Btns(props) {
+	const getItems = (item) => {
+		const panel = item.current;
+		const panel_items = panel.children;
+		const items = Array.from(panel_items);
+		const len = items.length;
+		const current_item = panel.querySelector('.on');
+		const current_index = items.indexOf(current_item);
+		return [panel, panel_items, len, current_item, current_index];
+	};
+
+	const moveUp = () => {
+		const [panel, panel_items, len, current_item, current_index] = getItems(props.panel);
+		console.log('panel', panel);
+		console.log('panel_items', panel_items);
+		console.log('len', len);
+		console.log('current_item', current_item);
+		console.log('current_index', current_index);
+	};
 	return (
 		<>
-			<a href='#' className='btnUp'>
+			<a href='#' className='btnUp' onClick={moveUp}>
 				<span></span>
 				<em>UP</em>
 			</a>
